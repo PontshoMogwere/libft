36cfa8726d878d8503652bc750293c54213d8f3b
Pontsho <pontsho6@live.com>
2019-05-19 17:15:19 +0200

    libft.h mod

--- libft.h
+++ libft.h
@@ -10,7 +10,7 @@ void		ft_putstr(char *str);
 int		ft_strcmp(char *s1, char *s2);
 size_t		ft_strlen(const char *s);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
-
+void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
 
 
 
