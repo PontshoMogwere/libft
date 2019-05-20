1c2b5d71e01803aa9d74b3e6f30a4fa979b700a8
Pontsho <pontsho6@live.com>
2019-05-20 04:18:40 +0200

    libft.h

--- libft.h
+++ libft.h
@@ -14,7 +14,7 @@ void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void		*ft_memchr(const void *s, int c, size_t);
 char		*ft_strdup(const char *s);
 int		ft_tolower(int c);
-int		ft_toupper(int c0;
+int		ft_toupper(int c);
 
 
 #endif
