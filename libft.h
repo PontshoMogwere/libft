4cba7898a8c7a9914f3196a2c770c19a14a98a3d
Pontsho MOGWERE <pmogwere@c5r6s11.wethinkcode.co.za>
2019-05-20 08:20:53 +0200

    libft.h Mod

--- libft.h
+++ libft.h
@@ -7,7 +7,7 @@
 void		ft_bzero(void *s, size_t n);
 void		ft_putchar(char c);
 void		ft_putstr(char *str);
-int		ft_strcmp(char *s1, char *s2);
+int		ft_strcmp(unsigned char *s1, unsigned char *s2);
 size_t		ft_strlen(const char *s);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
 void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
@@ -20,6 +20,6 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isascii(int c);
 int		ft_isalnum(int c);
-int		ft_strncmp(const char * s1, const char *s1, siz_t n); 
+int		ft_strncmp(const char * s1, const char *s2, size_t n); 
 
 #endif
