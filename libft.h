acfd777d1d361f9d937dc1980ad06929a902d348
Pontsho <pontsho6@live.com>
2019-05-20 05:32:58 +0200

    libft.h Mod

--- libft.h
+++ libft.h
@@ -19,5 +19,7 @@ int		ft_isprint(int c);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isascii(int c);
+int		ft_isalnum(int c);
+int		ft_strncmp(const char * s1, const char *s1, siz_t n); 
 
 #endif
