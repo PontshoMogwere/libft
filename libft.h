e4f5b0543a2d55185e42415ce2116b4db06d1a11
Pontsho MOGWERE <pmogwere@c5r6s11.wethinkcode.co.za>
2019-05-20 11:24:24 +0200

    libft.h

--- libft.h
+++ libft.h
@@ -21,5 +21,5 @@ int		ft_isdigit(int c);
 int		ft_isascii(int c);
 int		ft_isalnum(int c);
 int		ft_strncmp(const char * s1, const char *s2, size_t n); 
-
+char		*ft_strstr(const char *haystack, const char *needle);
 #endif
