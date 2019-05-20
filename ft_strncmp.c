f0f03a1d862cf786c634b533ac62c07a3b02bb80
Pontsho MOGWERE <pmogwere@c5r6s11.wethinkcode.co.za>
2019-05-20 08:20:03 +0200

    ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,6 +1,17 @@
-#include "libft.h"
+#include "libft.c"
 
-int	ft_strncpm(const char *s1, const char *s2, size_t n)
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	int i;
 
+	i = 0;
+	while (s1[i] != '\0' && n != 0)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
 }
