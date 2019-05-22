a54affbfc7d8ec8ba61aa102e323a12bd5f2a864
PontshoGj <50702476+PontshoGj@users.noreply.github.com>
2019-05-22 16:59:01 +0200

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,53 +1,22 @@
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-size_t	ft_strlen(const char *s);
-
-void	hangeNum(char *dest)
-{
-	char *c;
-
-	c = (char *)malloc(sizeof(char));
-	dest = c;
-	free(c);
-}
+#include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	const char *d;
-	char *c;
+	unsigned char *d;
+	unsigned char *c;
 	int i;
 
 	i = 0;
-	d = (char *)src;
-	//c = (char *)malloc(n * sizeof(d));
-	printf("a");
-	//hangeNum((char *)dest);
-	//c = (char *)dest;
-	//c = (char *)malloc(n * sizeof(d));
-	if (n > 0)
+	d = (unsigned char *)src;
+	c = (unsigned char *)dst;
+	if (dest > src)
 	{
-		printf("%s", d);
 		while (n-- != 0)
 		{
+			c += n;
 			c[i] = d[i];
 			i++;
 		}
-	//	c[i] = '\0';
 	}
 	return (dest);
-}
-
-
-int main(void)
-{
-	char d[50]  = "Hello World";
-	char *c;
-	
-	printf("%d", &c);
-
-	//ft_memmove(c, d, 6);
-	//printf("%s\n", c);
-	return (0);
 }
