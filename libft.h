79069e6173d0ee7ea4498be67e7bac7e0b787e16
Pontsho MOGWERE <pmogwere@c5r6s11.wethinkcode.co.za>
2019-05-23 14:16:27 +0200

    libft.h Mod

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pmogwere <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 13:29:33 by pmogwere          #+#    #+#             */
-/*   Updated: 2019/05/22 07:34:21 by pmogwere         ###   ########.fr       */
+/*   Updated: 2019/05/23 13:54:19 by pmogwere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_FILE
@@ -24,6 +24,7 @@ void		*ft_memset(void *str, int c, size_t len);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
 void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void		*ft_memchr(const void *s, int c, size_t);
+void		*ft_memmov(void *dest, const void *src, size_t n);
 char		*ft_strdup(const char *s);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
@@ -34,13 +35,16 @@ int		ft_isascii(int c);
 int		ft_isalnum(int c);
 int		ft_strncmp(const char * s1, const char *s2, size_t n); 
 char		*ft_strstr(const char *haystack, const char *needle);
+char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr(const char *s, int c);
 char		*ft_strcat(char *restrict s1, const char *restrict s2); 
 char		*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
 char		*ft_strcpy(char *dst, const char *src);
 char		*ft_strncpy(char *dst, const char *src, size_t len);
-
-
+void		ft_putendl(char const *s);
+void		*ft_memalloc(size_t size);
+int		ft_atoi(const char *str);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
