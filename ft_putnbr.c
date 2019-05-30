bd1196a4b79cf565fad5ab5b01699413321bba72
Pontsho <pontsho@b.localdomain>
2019-05-30 23:35:00 +0200

    Mod

---
+++ ft_putnbr.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+void		ft_putnbr(int n)
+{
+	int i;
+	char a;
+
+	i = n % 10;
+	a = '0';
+	if (n > 2147483647 || n < -2147483647)
+		return; 
+	if (i < 0)
+	{
+		ft_putchar('-');
+		n *= -1;
+		ft_putnbr(n);
+	}
+	if ( i > 0)
+	{
+		ft_putnbr(n / 10);
+		ft_putchar(i + '0');
+	}
+}
---
+++ ft_putnbr_fd.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+void		ft_putnbr_fd(int n, int fd)
+{
+	int i;
+	char a;
+
+	i = n % 10;
+	a = '0';
+	if (n > 2147483647 || n < -2147483647)
+		return; 
+	if (i < 0)
+	{
+		ft_putchar('-');
+		n *= -1;
+		ft_putnbr(n, fd);
+	}
+	if ( i > 0)
+	{
+		ft_putnbr_fd(n / 10);
+		ft_putchar_fd(i + '0', fd);
+	}
+}
