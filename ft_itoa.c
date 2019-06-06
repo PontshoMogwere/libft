d750443c2f562b8ed4936e4acb700c928c0063d5
Pontsho MOGWERE <pmogwere@c5r8s11.wethinkcode.co.za>
2019-06-06 17:39:14 +0200

    Mods

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,40 +1,72 @@
 #include "libft.h"
 #include <stdio.h>
-
-static char *lmin(void)
+static char *lmin(int n)
 {
 	char *s;
 
-	s = (char *)malloc(sizeof(char) * 20);
-	s = ft_strcpy(s, "-2147483648");
+	if (!(s = (char *)malloc(sizeof(char) * 20)))
+		return NULL;
+	if (n == (-2147483647 -1))
+		return (s = ft_strcpy(s, "-2147483648"));
+	if (n == 2147483647)
+		return (s = ft_strcpy(s, "2147483647"));
 	return (s);
+}
+
+static int	countint(int n)
+{
+	int count;
+
+	count = 1;
+	if (n < 0)
+		count++;
+	if (n == 0)
+		return (count);
+	while (n != 0)
+	{
+		count++;
+		n = n / 10;
+	}
+	return (count);
 }
 
 char		*ft_itoa(int n)
 {
 	int i;
-	int c;
+	char *c;
 	char *str;
-
+	
 	i = n % 10;
-	c = 0;
-	if (!(str = (char *)malloc(sizeof(char *) * 20)))
+	c = (char *)malloc(sizeof(char) * 2);
+	if ((!(str = (char *)malloc(sizeof(char) * countint(n)))) || c == NULL)
 			return (NULL);
-	if (n <= (-2147483647 -1))
-		str = lmin();
+	if ((n == (-2147483647 -1)) || (n >= 2147483647 || n < (-2147483647 -1)))
+		return (str = lmin(n));
 	if (n == 0)
-		str[c++] = '0';
+		return (str = "0");
 	if (i < 0)
 	{
-		str[c++] = '-';
+		str[0] = '-';
 		n *= -1;
 		ft_itoa(n);
 	}
-	if (i >= 0 && n / n  != 0)
+	if (i >= 0 && n != 0)
 	{
-		ft_itoa(n / 10);
-		str[c++] = i + '0';
+		str = ft_itoa(n / 10);;
+		*c++ = i + '0';
+		*c = '\0';
+		str = ft_strcat(str, c);
+		printf("%s\n", c);
 	}
-	str[c] = '\0';
 	return (str);
+}
+
+int main()
+{
+	int size;
+	char *i1;
+
+	i1 = ft_itoa(-50859);
+	printf("\n\n%s", i1);
+	return (0);
 }
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: pmogwere <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 09:53:15 by pmogwere          #+#    #+#             */
-/*   Updated: 2019/06/03 13:37:58 by pmogwere         ###   ########.fr       */
+/*   Updated: 2019/06/06 10:40:00 by pmogwere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,17 @@ void		ft_putnbr_fd(int n, int fd)
 
 	i = n % 10;
 	if (n > 2147483647 || n < (-2147483647 -1))
-		return;
+		return ;
 	if (n == (-2147483647 -1))
-		ft_putstr("-2147483648");
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
 	if (n == 0)
+	{
 		ft_putchar_fd('0', fd);
+		return ;
+	}
 	if (i < 0)
 	{
 		ft_putchar_fd('-', fd);
